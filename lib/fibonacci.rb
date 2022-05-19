4607b771d6541f05ecf424653b9d212f15b8e714
Nikolay Zhurilo <n.zhurilo@sigma-it.ru>
2022-05-19 08:59:17 +0300

    Add Binet's Formula to Fibonacci

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -15,6 +15,7 @@ class Fibonacci
     Hash.new { |h, k| h[k] = k < 2 ? k : h[k-1] + h[k-2] }[n]
   end
 
+  # for n = 40 execution time > 10s
   def recursion(n)
     n < 2 ? n : recursion(n - 1) + recursion(n - 2)
   end
@@ -22,5 +23,12 @@ class Fibonacci
   @@cache = [0, 1]
   def recursion_with_cache(n)
     @@cache[n] ||= recursion_with_cache(n - 1) + recursion_with_cache(n - 2)
+  end
+
+  # using Binet's Formula: http://mathworld.wolfram.com/BinetsFibonacciNumberFormula.html
+  # for number < 71
+  PHI = (1 + Math.sqrt(5)) / 2
+  def binets_formula(n)
+    (PHI**(n) / Math.sqrt(5)).round
   end
 end
--- spec/lib/fibonacci_spec.rb
+++ spec/lib/fibonacci_spec.rb
@@ -1,5 +1,12 @@
 RSpec.describe Fibonacci do
-  FIB_HASH = { 0 => 0, 2 => 1, 5 => 5, 99 => 218922995834555169026 }
+  FIB_HASH = {
+    0 => 0,
+    2 => 1,
+    5 => 5,
+    70 => 190392490709135,
+    71 => 308061521170129,
+    99 => 218922995834555169026
+  }
 
   let(:ex) { described_class.new }
 
@@ -28,7 +35,7 @@ RSpec.describe Fibonacci do
   end
 
   describe '#recursion' do
-    FIB_HASH.slice(0, 2, 5).each do |k, v|
+    FIB_HASH.select { |k, _| k < 40 }.each do |k, v|
       it "return #{v} for num #{k}" do
         expect(ex.recursion(k)).to eq v
       end
@@ -39,6 +46,14 @@ RSpec.describe Fibonacci do
     FIB_HASH.each do |k, v|
       it "return #{v} for num #{k}" do
         expect(ex.recursion_with_cache(k)).to eq v
+      end
+    end
+  end
+
+  describe '#binets_formula' do
+    FIB_HASH.select { |k, _| k < 71 }.each do |k, v|
+      it "return #{v} for num #{k}" do
+        expect(ex.binets_formula(k)).to eq v
       end
     end
   end
